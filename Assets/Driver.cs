566c0e24f4540a70dd996ee8d4475226ae06e76a
paigeberberick <paigeberberick7@gmail.com>
2026-09-14 11:38:57 -0400

    Added Rotations to Capsule (Eatty McDash)

--- Assets/Driver.cs
+++ Assets/Driver.cs
@@ -5,12 +5,14 @@ public class Driver : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+       
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Rotate(0, 0, 0.2f);
+        transform.Translate(0, 0.1f, 0);
+
     }
 }
