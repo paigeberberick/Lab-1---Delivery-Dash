e180e57bed07a3e91846ad613deee99e34660fc4
paigeberberick <paigeberberick7@gmail.com>
2026-09-15 11:28:58 -0400

    Added Variables

--- Assets/Driver.cs
+++ Assets/Driver.cs
@@ -2,6 +2,8 @@ using UnityEngine;
 
 public class Driver : MonoBehaviour
 {
+    float steerSpeed = .5f;
+    float moveSpeed = 0.1f; 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,8 +13,8 @@ public class Driver : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate(0, 0, 0.2f);
-        transform.Translate(0, 0.1f, 0);
+        transform.Rotate(0, 0, steerSpeed);
+        transform.Translate(0, moveSpeed, 0);
 
     }
 }
