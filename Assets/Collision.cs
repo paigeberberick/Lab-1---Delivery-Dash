0454b20ecbe8caa795ae9faed0a0a33791bafab1
paigeberberick <paigeberberick7@gmail.com>
2026-09-15 13:00:16 -0400

    Added obstatcle that will destroy player and itself

--- Assets/Collision.cs
+++ Assets/Collision.cs
@@ -5,10 +5,22 @@ public class Collision : MonoBehaviour
     private void OnCollisionEnter2D(Collision2D collision)
     {
         Debug.Log("Hit object!");
+
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            Destroy(collision.gameObject);
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         Debug.Log("Hit a checkpoint!");
+
+        if (collision.CompareTag("EvilObstacle"))
+        {
+            Destroy(collision.gameObject);
+        }
     }
 }
+
+
