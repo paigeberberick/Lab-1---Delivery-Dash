b68c937f5c832f81ac30b63a748f2059deb37da2
paigeberberick <paigeberberick7@gmail.com>
2026-09-22 23:42:07 -0400

    Added object destruction

--- Assets/Delivery.cs
+++ Assets/Delivery.cs
@@ -3,13 +3,14 @@ using UnityEngine;
 public class Delivery : MonoBehaviour
 {
     bool hasPackage;
-
+    [SerializeField] float delay = 1f;
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.CompareTag("Package"))
+        if (collision.CompareTag("Package") && !hasPackage)
         {
             Debug.Log("Picked up Package!");
             hasPackage = true;
+            Destroy(collision.gameObject, delay);
         }
 
         if (collision.CompareTag("Customer") && hasPackage)
